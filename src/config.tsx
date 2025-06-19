6d82366f86d2d0546c80f489ee67c98fe14c0d2f
JohnPetersenDenmark <johnpetersen1959@gmail.com>
2025-06-19 16:05:36 +0200

    ok

--- src/config.tsx
+++ src/config.tsx
@@ -1,10 +1,10 @@
 
 const config = {
 
-  API_BASE_URL: "http://192.168.8.105:5000"
+  //API_BASE_URL: "http://192.168.8.105:5000"
 
 
-  // API_BASE_URL: "https://mackiewebapi.fly.dev"
+  API_BASE_URL: "https://api.dotnetdev.dk"
 };
 
 export default config;
